4a4d129023cd91e075e5ae8f3b4c44f40ad1e2c6
Sanchit Ahuja <t-sahuja@microsoft.com>
2023-09-04 19:35:23 +0000

    added tamil nadu clues and some cleanup

--- src/opensource/templates.py
+++ src/opensource/templates.py
@@ -12,5 +12,28 @@ bot:
 
 
 INST_LLAMA_TEMPLATE = """
-    [INST] Name the object on the basis of the above clues. After your guess, I will let you know if you are correct or not. Accordingly, make your second and final guess. I do not need to know your reasoning behind the answer. Just tell me the answer. If you do not know the answer, say that you do not know the answer.[/INST]
+    [INST] Name the object on the basis of the above clues. After your guess, I will let you know if you are correct or not. If you're correct, do not make any further guesses and end the conversation. If you are wrong, make your second and final guess. I do not need to know your reasoning behind the answer. Just tell me the answer. If you do not know the answer, say that you do not know the answer.[/INST]
+"""
+
+SYS_LLAMA_TRANSLATE_TEMPLATE = """
+    <s> <<SYS>>\n
+    INSTRUCTION: You are a helpful assistant that translates from Hindi to English.\nNote that the query is code-mixed, so you may need to translate some words from Hindi to English and leave others as they are.\nNow, translate the given source sentence from Hindi to English. I do not need any specific notes from you regarding your translation. If you cannot translate it, do not try to make up an answer, just say '**********'.
+    {chat_history}
+    <</SYS>>\n
+[INST]
+Human: {human_input}
+[/INST]
+bot:
+"""
+
+SYS_BLOOMZ_TEMPLATE = """
+     You are an agent who is well versed with the cultures of the world. You are playing a game of taboo with another agent who is also well- versed with the cultures of the world. You can only make two guesses to correctly guess this social artifact and you cannot ask any clarification questions. Social artifacts are objects that helps us connect and stay associated with the culture. These objects are known and have a significance to most people who consider themselves as a part of that culture and serve as way of identifying ourselves with the culture and the people in that culture. Your clues are \n {cluelist}.\n 
+    {chat_history} \n
+
+    Human: {human_input}
+
+"""
+
+INST_BLOOMZ_TEMPLATE = """
+    Name the object on the basis of the above clues. After your guess, I will let you know if you are correct or not. If you're correct, do not make any further guesses and end the conversation. If you are wrong, make your second and final guess. I do not need to know your reasoning behind the answer. Just tell me the answer. If you do not know the answer, say that you do not know the answer.
 """
