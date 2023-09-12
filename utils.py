96a6b41e02ca37de92c3a3638565815e9dbb4b85
Sanchit Ahuja <sanchitahuja205@gmail.com>
2023-09-12 13:35:14 +0000

    refactor mostly

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 
 def get_clues(clue_path):
     with open(clue_path, "r") as f:
@@ -6,3 +7,7 @@ def get_clues(clue_path):
     for i, clue in enumerate(clues):
         output += f"CLUE-{i+1}: {clue}\n"
     return output.strip()
+
+def generate_abs_path(relative_path):
+    file_abs_path = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(file_abs_path,relative_path)
