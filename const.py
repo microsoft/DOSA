b6d9596ee4244aa9de79b9fa8d70bd47768c73cb
Sanchit Ahuja <t-sahuja@microsoft.com>
2023-10-05 11:20:37 +0000

    added results and made changes to const

--- const.py
+++ const.py
@@ -1,56 +1,69 @@
 from utils import generate_abs_path
 
 STATE_CLUES_NOTES_DICT = {
+    "haryana": [
+        generate_abs_path("clues/haryana/clues.csv"),
+        generate_abs_path("clues/haryana/notes.csv"),
+    ],
+    "telangana": [
+        generate_abs_path("clues/telangana/clues.csv"),
+        generate_abs_path("clues/telangana/notes.csv"),
+    ],
+    "west_bengal": [
+        generate_abs_path("clues/west_bengal/clues.csv"),
+        generate_abs_path("clues/west_bengal/notes.csv"),
+    ],
     "jharkhand": [
-        generate_abs_path("clues/jharkhand/artifact_clues.csv"),
+        generate_abs_path("clues/jharkhand/clues.csv"),
         generate_abs_path("clues/jharkhand/notes.csv"),
     ],
     "assam": [
-        generate_abs_path("clues/assam/artifacts_clues.csv"),
+        generate_abs_path("clues/assam/clues.csv"),
         generate_abs_path("clues/assam/notes.csv"),
     ],
-    "gujarat": [generate_abs_path("clues/gujarat/gujarat_clues.csv")],
+    "gujarat": [
+        generate_abs_path("clues/gujarat/clues.csv"),
+        generate_abs_path("clues/gujarat/notes.csv"),
+    ],
     "karnataka": [
-        generate_abs_path("clues/karnataka/karnataka_clues.csv"),
-        generate_abs_path("clues/karnataka/karnataka_notes.csv"),
+        generate_abs_path("clues/karnataka/clues.csv"),
+        generate_abs_path("clues/karnataka/notes.csv"),
     ],
     "maharashtra": [
-        generate_abs_path("clues/maharashtra/maharashtra_clues.csv"),
-        generate_abs_path("clues/maharashtra/maharashtra_notes.csv"),
+        generate_abs_path("clues/maharashtra/clues.csv"),
+        generate_abs_path("clues/maharashtra/notes.csv"),
     ],
     "odisha": [
-        generate_abs_path("clues/odisha/artifact_clues.csv"),
+        generate_abs_path("clues/odisha/clues.csv"),
         generate_abs_path("clues/odisha/notes.csv"),
     ],
     "punjab": [
-        generate_abs_path("clues/punjab/punjab_clues.csv"),
-        generate_abs_path("clues/punjab/punjab_notes.csv"),
+        generate_abs_path("clues/punjab/clues.csv"),
+        generate_abs_path("clues/punjab/notes.csv"),
     ],
     "tamil_nadu": [
-        generate_abs_path("clues/tamil_nadu/tamil_clues.csv"),
-        generate_abs_path("clues/tamil_nadu/tamil_notes.csv"),
+        generate_abs_path("clues/tamil_nadu/clues.csv"),
+        generate_abs_path("clues/tamil_nadu/notes.csv"),
     ],
-    "telangana": [
-        generate_abs_path("clues/telangana/clues_artifact.csv"),
-        generate_abs_path("clues/telangana/notes.csv"),
-    ],
-    "west_bengal": [generate_abs_path("clues/west_bengal/west_bengal_clues.csv")],
     "kerala": [
-        generate_abs_path("clues/kerala/kerala_clues.csv"),
-        generate_abs_path("clues/kerala/kerala_notes.csv"),
+        generate_abs_path("clues/kerala/clues.csv"),
+        generate_abs_path("clues/kerala/notes.csv"),
     ],
-    "chattisgarh": [generate_abs_path("clues/chhattisgarh/artifact_clues.csv")],
-    "delhi": [generate_abs_path("clues/delhi/delhi_clues.csv")],
+    "chattisgarh": [
+        generate_abs_path("clues/chhattisgarh/clues.csv"),
+        generate_abs_path("clues/chhattisgarh/notes.csv"),
+    ],
+    "delhi": [generate_abs_path("clues/delhi/clues.csv")],
     "bihar": [
-        generate_abs_path("clues/bihar/bihar_clues.csv"),
-        generate_abs_path("clues/bihar/bihar_notes.csv"),
+        generate_abs_path("clues/bihar/clues.csv"),
+        generate_abs_path("clues/bihar/notes.csv"),
     ],
     "andhra_pradesh": [
-        generate_abs_path("clues/andhra_pradesh/andhra_clues.csv"),
-        generate_abs_path("clues/andhra_pradesh/notes_clues.csv"),
+        generate_abs_path("clues/andhra_pradesh/clues.csv"),
+        generate_abs_path("clues/andhra_pradesh/notes.csv"),
     ],
     "uttar_pradesh": [
-        generate_abs_path("clues/uttar_pradesh/up_clues.csv"),
-        generate_abs_path("clues/uttar_pradesh/up_notes.csv"),
+        generate_abs_path("clues/uttar_pradesh/clues.csv"),
+        generate_abs_path("clues/uttar_pradesh/notes.csv"),
     ],
 }
